114968a5506bc4f2c0e189093be2847abdb59dc3
cowan <cowan@DESKTOP-E1DT78U>
2022-07-21 17:18:46 +0300

    Add project files.

---
+++ SifreKontrolOdev/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SifreKontrolOdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPower = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(28, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şifre:";
+            // 
+            // lblPower
+            // 
+            this.lblPower.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblPower.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPower.ForeColor = System.Drawing.Color.White;
+            this.lblPower.Location = new System.Drawing.Point(71, 65);
+            this.lblPower.Name = "lblPower";
+            this.lblPower.Size = new System.Drawing.Size(161, 24);
+            this.lblPower.TabIndex = 1;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(71, 30);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(161, 23);
+            this.txtSifre.TabIndex = 2;
+            this.txtSifre.TextChanged += new System.EventHandler(this.txtSifre_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 178);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.lblPower);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPower;
+        private System.Windows.Forms.TextBox txtSifre;
+    }
+}
+
---
+++ SifreKontrolOdev/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SifreKontrolOdev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string sifre = "";
+        private void txtSifre_TextChanged(object sender, EventArgs e)
+        {
+            sifre = txtSifre.Text;
+            if (sifre == "")
+            {
+                
+                lblPower.ForeColor = Color.Black;
+                lblPower.BackColor = Color.White;
+                txtSifre.BackColor = Color.White;
+                lblPower.Text = "Şifrenizi Giriniz";
+            }
+            else if (sifre.Length < 8)
+            {
+                lblPower.BackColor = Color.Red;
+                txtSifre.BackColor = Color.Red;
+                lblPower.Text = "Zayıf";
+                lblPower.ForeColor = Color.White;
+                txtSifre.ForeColor = Color.White;
+
+
+            }
+            else if (sifre.Length >= 8)
+            {
+                if (sifre.All(Char.IsDigit) || sifre.All(Char.IsLetter) || !sifre.Any(Char.IsLetterOrDigit))
+                {
+                    lblPower.BackColor = Color.Red;
+                    lblPower.Text = "Zayıf";
+                    txtSifre.BackColor = Color.Red;
+                    lblPower.ForeColor = Color.White;
+                    txtSifre.ForeColor = Color.White;
+
+
+                }
+                else if (sifre.Any(Char.IsDigit) && sifre.Any(Char.IsLetter) && !sifre.All(Char.IsLetterOrDigit) && sifre.Any(Char.IsUpper))
+                {
+                    lblPower.BackColor = Color.Green;
+                    lblPower.Text = "Güçlü";
+                    txtSifre.BackColor = Color.Green;
+                    lblPower.ForeColor = Color.White;
+                    txtSifre.ForeColor = Color.White;
+
+
+                }
+
+                else
+                {
+                    lblPower.BackColor = Color.Blue;
+                    lblPower.Text = "Orta";
+                    txtSifre.BackColor = Color.Blue;
+                    lblPower.ForeColor = Color.White;
+                    txtSifre.ForeColor = Color.White;
+                }
+            }
+        }
+    }
+}
